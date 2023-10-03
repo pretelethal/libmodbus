9e4fe65111026f147952bb752c5b7987fb31afe7
Jay Stoffle <jaystoffle@csquaredsystems.com>
2023-10-03 12:08:02 -0400

    restore missing consts?

--- src/modbus-tcp-private.h
+++ src/modbus-tcp-private.h
@@ -27,6 +27,9 @@ typedef struct _modbus_tcp {
     char ip[16];
 } modbus_tcp_t;
 
+#define _MODBUS_TCP_PI_NODE_LENGTH    1025
+#define _MODBUS_TCP_PI_SERVICE_LENGTH   32
+
 typedef struct _modbus_tcp_pi {
     /* Transaction ID */
     uint16_t t_id;
